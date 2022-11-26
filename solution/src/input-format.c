db75fbee7b3194a6f6bed69d8033daf088474ee8
grxuau <henryycampbell@gmail.com>
2022-11-26 15:58:33 +0300

    fix functions

--- solution/src/input-format.c
+++ solution/src/input-format.c
@@ -1,6 +1,4 @@
-#include <bmp.h>
 #include <stdio.h>
-#include <input-format.h>
 
 enum read_status  {
     READ_OK = 0,
@@ -21,32 +19,35 @@ enum open_status {
 
 enum close_status {
     CLOSE_OK = 0,
-    CLOSE_ERROR_PROCESS_IS_RUNNING,
     CLOSE_ERROR
 };
 
-enum read_statuc open_bmp(FILE* file, struct image const* img) {
+//TODO: решить, что возвращать в случае ошибки
+FILE* open_bmp(FILE* file, struct image const* img) {
     file = fopen("imageToOpen.bmp", "r");
     if (file == NULL) {
         int status = OPEN_ERROR;
         printf("Error! Status: %d", status);
+        return file;
     } else {
         int status = OPEN_OK;
         printf("File was opened successfully! Status: %d", status);
-
+        return file;
     }
 }
 
 enum close_status close_bmp(FILE* file, struct image const* img) {
-    if (fclose("imageToClose.bmp") == EOF) {
+    if (fclose(file)== EOF) {
         int status = CLOSE_ERROR;
         printf("An error occurred while closing the file! Status: %d", status);
     } else {
         int status = CLOSE_OK;
         printf("File was closed successfully! Status: %d", status);
+        return status;
     }
 }
 
+//TODO: сделать функцию чтения заголовков bmp-файла
 enum read_status from_bmp( FILE* in, struct image* img ) {
 
 }
