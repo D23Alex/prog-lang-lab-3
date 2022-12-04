df4f620aa42dbcff9b081bf809ae1cda37e3face
grxuau <henryycampbell@gmail.com>
2022-12-04 18:03:33 +0300

    add set header meethod & some additions

--- solution/src/image.c
+++ solution/src/image.c
@@ -1,11 +1,27 @@
 #include <stdint.h>
+#include <stdlib.h>
+#include <include/image.h>
 
-//TODO: учесть пиксель
+//FIXME: учесть пиксель
 uint8_t get_padding(uint32_t width) {
     uint8_t padding = 0;
     while ((width + padding) % 4 != 0) {
         padding ++;
     }
     return padding;
+}
+
+struct image create_image(uint64_t height, uint64_t width) {
+    void* data = malloc(width*height* (sizeof(struct pixel)));
+
+    if (data == NULL) {
+        return (struct image) {
+            0
+        };
+    } else {
+        return (struct image) {
+            width, height, data
+        };
+    }
 }
 
--- solution/src/input-format.c
+++ solution/src/input-format.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <include/bmp.h>
+#include <include/image.h>
 
 typedef struct bmp_header bmp_header;
 
 enum read_status  {
     READ_OK = 0,
-    READ_INVALID_HEADER
+    READ_INVALID_HEADER,
+    READ_INVALID_IMG
 };
 
 enum write_status  {
@@ -23,63 +26,89 @@ enum close_status {
     CLOSE_ERROR
 };
 
-enum read_status read_header(FILE* in) {
+struct header {
+    int valid;
     bmp_header bmpHeader;
-    if ((fread(&bmpHeader, sizeof (bmpHeader), 1, in)) != 1) {
-        int status = READ_INVALID_HEADER;
-        return status;
-    } else {
-        int status = READ_OK;
-        return status;
-    }
-}
+};
 
-//TODO: сделать здесь открытие файла
-//FIXME: CONST VOID
-enum write_status write_header(FILE* out, const void *bmpHeader) {
-    if ((fwrite(bmpHeader, sizeof(bmpHeader), 1, out)) != 1) {
-        int status = WRITE_ERROR;
-        return status;
-    } else {
-        int status = WRITE_OK;
-        return status;
-    }
-}
-
-
-
-//TODO: решить, что возвращать в случае ошибки
-//FIXME: неправильное открытие файла
-FILE* open_bmp(char fileName[]) {
-    FILE* bmpImage = fopen(fileName, "rb");
-    if (bmpImage == NULL) {
-        int status = OPEN_ERROR;
-        printf("Error! Status: %d", status);
-        return bmpImage;
-    } else {
+//TODO: на открытых файлах они могут запускать from_bmp и to_bmp
+enum open_status open_bmp(FILE* in) {
+    if (fopen((const char *) in, "rb") != NULL) {
         int status = OPEN_OK;
-        printf("File was opened successfully! Status: %d", status);
-        return bmpImage;
+        return status;
+    } else {
+        int status = OPEN_ERROR;
+        return status;
     }
 }
 
-//TODO: для чего нужен второй параметр?
-//ANSWER: чтобы туда записывать информацию об изображении!
-//TODO: нужно ли проверять, является ли файл формата BMP?
-enum close_status close_bmp(FILE* file) {
-    if (fclose(file) == EOF) {
-        int status = CLOSE_ERROR;
-        printf("An error occurred while closing the file! Status: %d", status);
-        return status;
-    } else {
+enum close_status close_bmp(FILE* in) {
+    if (fclose(in) != EOF) {
         int status = CLOSE_OK;
-        printf("File was closed successfully! Status: %d", status);
+        return status;
+    }  else {
+        int status = CLOSE_ERROR;
         return status;
     }
 }
+
+struct header read_header(FILE* in) {
+    bmp_header bmpHeader;
+    if (open_bmp(in) == 0) {
+        if (fread(&bmpHeader, sizeof bmpHeader, 1, in) != 1) {
+            return (struct header) {
+                    .valid = 0,
+                    .bmpHeader = bmpHeader
+            };
+        } else {
+            return (struct header) {
+                .valid = 1,
+                .bmpHeader = bmpHeader
+            };
+        }
+    } else {
+        return (struct header) {
+            .valid = 0,
+        };
+    }
+};
+
+//FIXME убрать заглушки
+struct bmp_header set_header(uint64_t width, uint64_t height) {
+    bmp_header bmpHeader;
+    bmpHeader.bfType = 0x4D42;
+    bmpHeader.bfileSize = width * sizeof(pixel) + sizeof(bmpHeader) + height;
+    bmpHeader.bfReserved = 0;
+    bmpHeader.bOffBits = 54;
+    bmpHeader.biSize = 40;
+    bmpHeader.biWidth = width;
+    bmpHeader.biHeight = height;
+    bmpHeader.biPlanes = 1;
+    bmpHeader.biBitCount = 24;
+    bmpHeader.biCompression = 0;
+    bmpHeader.biSizeImage;
+    bmpHeader.biXPelsPerMeter = 0;
+    bmpHeader.biYPelsPerMeter = 0;
+    bmpHeader.biClrUsed = 0;
+    bmpHeader.biClrImportant = 0;
+    return bmpHeader;
+
+}
+
+
 
 enum read_status from_bmp( FILE* in, struct image* img ) {
+    if (img == NULL) {
+        return READ_INVALID_IMG;
+    } else {
+       struct header header = read_header(in);
+       if (header.valid != 1) {
+           return READ_INVALID_HEADER;
+       } else {
+           bmp_header bmpHeader = header.bmpHeader;
 
+       }
+    }
 }
 
 enum write_status to_bmp( FILE* out, struct image const* img ) {
